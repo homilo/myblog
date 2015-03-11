16d50a802a02695867db094a937e6d0154f7e71a
Lê Trọng Lợi <trongloikt192@gmail.com>
2015-03-11 20:45:22 +0700

    Publish

---
+++ app/controllers/CommentsController.php
@@ -0,0 +1,92 @@
+<?php
+
+class CommentsController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 * GET /comments
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 * GET /comments/create
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /comments
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$validator = Comment::validate($data = Input::all());
+        if ($validator->fails())
+        {
+            return Redirect::back()->withErrors($validator)->withInput();
+        }
+        Comment::create($data);
+        return Redirect::route('posts.index')->withSuccess(Lang::get('larabase.comment_created'));
+	}
+
+	/**
+	 * Display the specified resource.
+	 * GET /comments/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 * GET /comments/{id}/edit
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 * PUT /comments/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 * DELETE /comments/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
